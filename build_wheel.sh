9b09892ceff5588f6ed9470eb5774ca5e487791e
Xavier Nodet <xavier.nodet@fr.ibm.com>
2022-02-10 13:52:08 +0100

    A bash script to build CPLEX and docplex wheels
    This script will grab the CPLEX and docplex packages from
    an installed copy of the software, and build wheel files
    specific to the combination of Python and platform.

---
+++ build_wheel.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e -x
+
+CPLEX_DIR=$CPLEX_STUDIO_DIR221
+PYTHON_VERSION=3.8
+PLATFORM=x86-64_osx
+
+python -m venv build_wheel_venv
+source build_wheel_venv/bin/activate
+pip install --upgrade pip
+pip install setuptools wheel
+
+rm -rf build
+mkdir build
+cd build
+
+cp -r $CPLEX_DIR/cplex/python/$PYTHON_VERSION/$PLATFORM cplex
+cd cplex
+sed -i .bak '1s/^/import setuptools\n/' setup.py
+python setup.py bdist_wheel
+cd ..
+
+cp -r $CPLEX_DIR/python/docplex .
+cd docplex
+sed -i .bak '1s/^/import setuptools\n/' setup.py
+python setup.py bdist_wheel
+cd ..
+
+cd ..
+mv build/cplex/dist/*.whl build/docplex/dist/*.whl .
+
+
+# Rename the wheels so that they have a name that really
+# fits their content
+PYTHON_VERSION_NO_DOT=${PYTHON_VERSION//./}
+PYTHON_PLATFORM=`python -c "import distutils.util
+print(distutils.util.get_platform())"`
+PYTHON_PLATFORM=${PYTHON_PLATFORM//./_}
+PYTHON_PLATFORM=${PYTHON_PLATFORM//-/_}
+for f in *.whl; do
+	mv $f `echo $f | sed "s/-py3-/-py$PYTHON_VERSION_NO_DOT-/"`
+done
+for f in *.whl; do
+	mv $f `echo $f | sed "s/any/$PYTHON_PLATFORM/"`
+done
